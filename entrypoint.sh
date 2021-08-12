26c437ad2d53454ab4c937d808af78c03a986762
huhaiqing <lixin@gainhon.com>
2021-08-12 17:05:47 +0800

    提交

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd /jsb
+cd /doplat
 export ENV_FILE=doplat/.prod
 
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple importlib-metadata==3.10.1
 pip install -i https://mirrors.aliyun.com/pypi/simple -r requirements.txt
 
 [ -f /var/run/celery/worker.pid ] && rm -f /var/run/celery/worker.pid
-celery multi start -A jsb worker -c 2 -l info -f logs/worker.log
-nohup celery -A jsb beat -l info -f logs/beat.log  >/dev/null 2>&1 &
+celery multi start -A doplat worker -c 2 -l info -f logs/worker.log
+nohup celery -A doplat beat -l info -f logs/beat.log  >/dev/null 2>&1 &
 # python manage.py runserver
-gunicorn jsb.wsgi --bind=0.0.0.0:8000 --log-file logs/INFO.log --workers 2
+gunicorn doplat.wsgi --bind=0.0.0.0:8000 --log-file logs/INFO.log --workers 2
