556e7a4ffc58a53a65a87269702b25bbfe355fd5
huhaiqing <lixin@gainhon.com>
2021-08-28 11:32:03 +0800

    提交

--- app/serializers/account.py
+++ app/serializers/account.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from app.models import Account
+from django.contrib.auth.models import Group
+from rest_framework_guardian.serializers import ObjectPermissionsAssignmentMixin
 
 
-class AccountSerializer(serializers.ModelSerializer):
+class AccountSerializer(ObjectPermissionsAssignmentMixin, serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = '__all__'
+
+    def get_permissions_map(self, created):
+        current_user = self.context['request'].user
+        current_groups = Group.objects.filter(user=current_user)
+        # 用户及所在的组
+        user_group = [current_user]
+        for current_group in current_groups:
+            user_group.append(current_group)
+
+        return {
+            'view_account': user_group,
+            'change_account': user_group,
+            'delete_account': user_group
+        }
