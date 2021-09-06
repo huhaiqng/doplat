aa41bb9ba7fa1626086c7efc623de3d4663dc1fc
huhaiqing <lixin@gainhon.com>
2021-09-06 10:35:43 +0800

    优化 contenttype api

--- authperm/urls.py
+++ authperm/urls.py
@@ -9,12 +9,12 @@ router.register(r'userinfo', UserInfoViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(r'l1menu', L1MenuViewSet)
 router.register(r'l2menu', L2MenuViewSet)
-router.register(r'contenttype', ContentTypeViewSet)
 router.register(r'groupobjectpermission', GroupObjectPermissionViewSet)
 
 urlpatterns = [
     path('getLoginUser/', GetLoginUser.as_view()),
     path('getUserHostedInfo/', GetUserHostedInfoViewSet.as_view()),
     path('getL2MenuContentType/', L2MenuContentTypeViewSet.as_view()),
-    path('permission/', PermissionViewSet.as_view())
+    path('permission/', PermissionViewSet.as_view()),
+    path('contenttype', ContentTypeViewSet.as_view())
 ]
--- authperm/views/contenttype.py
+++ authperm/views/contenttype.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
 from django.contrib.auth.models import ContentType
 from authperm.serializers import ContentTypeSerializer
+from rest_framework import generics
 
 
-class ContentTypeViewSet(viewsets.ModelViewSet):
+class ContentTypeViewSet(generics.ListAPIView):
     queryset = ContentType.objects.filter(app_label__in=['app', 'authperm', 'project'])
     serializer_class = ContentTypeSerializer
     pagination_class = None
