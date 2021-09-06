0784674e1f57cec4497e4684d478edb39c74771b
huhaiqing <lixin@gainhon.com>
2021-09-06 13:41:18 +0800

    合并 mysql-perm api 到 mysql

--- project/views/__init__.py
+++ project/views/__init__.py
@@ -1,6 +1,6 @@
 from .domain import GetLingfannaoDomainRecord, GetLingfannaoDomain, GetGaingon666Domain, GetGaingon666DomainRecord
 from .env import EnvViewSet
-from .mysql import MySQLViewSet, MySQLPermViewSet
+from .mysql import MySQLViewSet
 from .host import HostViewSet
 from .task_result import TaskResultViewSet
 from .config import ConfigViewSet, GetConfigViewSet, ConfigPermViewSet
--- project/views/mysql.py
+++ project/views/mysql.py
@@ -1,10 +1,10 @@
-from project.serializers import MySQLSerializer
-from project.models import MySQL
-from project.filters import MySQLFilter
 from rest_framework import viewsets
 from rest_framework.response import Response
 from guardian.models import GroupObjectPermission
+from project.models import MySQL
+from project.serializers import MySQLSerializer
 from authperm.serializers import GroupObjectPermissionSerializer
+from project.filters import MySQLFilter
 
 
 # MySQL 实例
@@ -13,26 +13,29 @@ class MySQLViewSet(viewsets.ModelViewSet):
     serializer_class = MySQLSerializer
     filterset_class = MySQLFilter
 
-
-# MySQL Perm
-class MySQLPermViewSet(viewsets.ModelViewSet):
-    queryset = MySQL.objects.all()
-    serializer_class = MySQLSerializer
-    filterset_class = MySQLFilter
-
     def list(self, request, *args, **kwargs):
+        with_perms = request.query_params.get('with_perms', 'false')
         content_type = request.GET.get('content_type')
         group = request.GET.get('group')
         queryset = self.filter_queryset(self.get_queryset())
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
+            # 设置对象权限需要对象权限
+            if with_perms == 'true':
+                for h in serializer.data:
+                    perms = GroupObjectPermission.objects.filter(object_pk=h['id'], content_type=content_type, group=group)
+                    h['perms_detail'] = GroupObjectPermissionSerializer(perms, many=True).data
+                    h['perms'] = perms.values_list('permission', flat=True)
+                    h['name'] = h.pop('inside_addr')
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        # 设置对象权限需要对象权限
+        if with_perms == 'true':
             for h in serializer.data:
                 perms = GroupObjectPermission.objects.filter(object_pk=h['id'], content_type=content_type, group=group)
                 h['perms_detail'] = GroupObjectPermissionSerializer(perms, many=True).data
                 h['perms'] = perms.values_list('permission', flat=True)
                 h['name'] = h.pop('inside_addr')
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
