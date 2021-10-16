3b8c7403ec55a42a9e7f57a9a7a31bcebeb7e769
huhaiqing <lixin@gainhon.com>
2021-10-16 10:26:04 +0800

    middleware 排序

--- project/models/url.py
+++ project/models/url.py
@@ -1,15 +1,20 @@
 from django.db import models
 from .project import Project
 from .env import Env
+from .host import Host
 
 
 # 地址
 class Url(models.Model):
     name = models.CharField('名称', max_length=200)
+    alias = models.CharField('简称', max_length=200, blank=True, null=True)
+    hosts = models.ManyToManyField(Host, verbose_name='主机', blank=True, related_name='host_url')
+    deploy_dir = models.CharField('部署路径', max_length=200, blank=True, null=True)
     project = models.ForeignKey(Project, verbose_name='项目', on_delete=models.PROTECT, related_name='urls')
     env = models.ForeignKey(Env, verbose_name='环境', on_delete=models.PROTECT)
     url = models.URLField('地址', max_length=200, unique=True)
     popular = models.BooleanField('常用的', default=False)
+    public_ip = models.GenericIPAddressField('公网 IP', blank=True, null=True)
 
     class Meta:
         ordering = ['project', 'env', 'name']
--- sqlmigrate.sql
+++ sqlmigrate.sql
@@ -1,2 +1,23 @@
-CREATE TABLE `project_gitlabrepo` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `name` varchar(255) NOT NULL, `description` varchar(255) NULL, `http_url_to_repo` varchar(200) NOT NULL, `created_at` datetime(6) NULL, `last_activity_at` datetime(6) NULL, `project_id` integer NULL);
-ALTER TABLE `project_gitlabrepo` ADD CONSTRAINT `project_gitlabrepo_project_id_4e8ec259_fk_project_project_id` FOREIGN KEY (`project_id`) REFERENCES `project_project` (`id`);
+--
+-- Change Meta options on middleware
+--
+--
+-- Add field alias to url
+--
+ALTER TABLE `project_url` ADD COLUMN `alias` varchar(200) NULL;
+--
+-- Add field deploy_dir to url
+--
+ALTER TABLE `project_url` ADD COLUMN `deploy_dir` varchar(200) NULL;
+--
+-- Add field hosts to url
+--
+CREATE TABLE `project_url_hosts` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `url_id` integer NOT NULL, `host_id` integer NOT NULL);
+ALTER TABLE `project_url_hosts` ADD CONSTRAINT `project_url_hosts_url_id_host_id_7ccbdf4d_uniq` UNIQUE (`url_id`, `host_id`);
+ALTER TABLE `project_url_hosts` ADD CONSTRAINT `project_url_hosts_url_id_3467774b_fk_project_url_id` FOREIGN KEY (`url_id`) REFERENCES `project_url` (`id`);
+ALTER TABLE `project_url_hosts` ADD CONSTRAINT `project_url_hosts_host_id_df11cbfb_fk_project_host_id` FOREIGN KEY (`host_id`) REFERENCES `project_host` (`id`);
+
+--
+-- Add field public_ip to url
+--
+ALTER TABLE `project_url` ADD COLUMN `public_ip` char(39) NULL;
