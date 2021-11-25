76aeebb5bf46b1cdad8a584102edb751c8db745d
huhaiqing <lixin@gainhon.com>
2021-11-25 10:51:54 +0800

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 cd /doplat
 export DOPLAT_ENV_FILE=doplat/.dev
 
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple importlib-metadata==3.10.1
-pip install -i https://mirrors.aliyun.com/pypi/simple -r requirements.txt
+# pip install -i https://pypi.tuna.tsinghua.edu.cn/simple importlib-metadata==3.10.1
+# pip install -i https://mirrors.aliyun.com/pypi/simple -r requirements.txt
 
 [ -f /var/run/celery/worker.pid ] && rm -f /var/run/celery/worker.pid
 celery multi start -A doplat worker -c 2 -l info -f logs/worker.log
